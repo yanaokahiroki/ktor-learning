a622ef13fda907484f3f6f17b95207522e8d28aa
Hiroki Yanaoka <40460282+yanaokahiroki@users.noreply.github.com>
2023-02-23 17:24:23 +0900

    :sparkles: 注文APIを追加
    * listOrders: 全注文を取得
    * getOrder: 指定した注文を取得
    * totalizeOrder: 指定した注文の合計金額を取得

--- src/main/kotlin/com/learning/plugins/Routing.kt
+++ src/main/kotlin/com/learning/plugins/Routing.kt
@@ -1,11 +1,17 @@
 package com.learning.plugins
 
 import com.learning.routes.customerRouting
+import com.learning.routes.getOrderRoute
+import com.learning.routes.listOrdersRoute
+import com.learning.routes.totalizeOrderRoute
 import io.ktor.server.application.Application
 import io.ktor.server.routing.routing
 
 fun Application.configureRouting() {
   routing {
     customerRouting()
+    listOrdersRoute()
+    getOrderRoute()
+    totalizeOrderRoute()
   }
 }
